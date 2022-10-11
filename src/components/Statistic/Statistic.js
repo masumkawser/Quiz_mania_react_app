d7ade0a377d59a8b3fd84bc815935821da00eca0
masumkawser <kawser151722@gmail.com>
2022-10-11 19:33:32 +0600

    layout added

---
+++ src/components/Statistic/Statistic.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Statistic = () => {
+    return (
+        <div>
+            <h2> this is stastics</h2>
+        </div>
+    );
+};
+
+export default Statistic;
