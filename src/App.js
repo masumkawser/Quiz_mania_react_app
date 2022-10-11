d8aacb836212e3288d600ff93b5dd52bb7190ecd
masumkawser <kawser151722@gmail.com>
2022-10-11 20:27:21 +0600

    nav added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css';
+// import './App.css';
+import Blog from './components/Blog/Blog';
+import Home from './components/Home/Home';
+import Statistic from './components/Statistic/Statistic';
+import Main from './layout/Main';
 
 function App() {
   const router=createBrowserRouter([
 
     {
       path:'/',
-      element:
+      element:<Main></Main>,
+      children:[
+        {
+          path:'/',
+         element:<Home></Home>
+        },
+        {
+          path:'/statistic',
+         element:<Statistic></Statistic>
+        },
+        {
+          path:'/blog',
+         element:<Blog></Blog> },
+      ]
+    },
+    {
+      path: '*',
+      element: <div>This path not found!</div>
     }
   ])
   return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import"./Header.css";
 const Header = () => {
     return (
-        <div>
-            <h2> this is header...</h2>
+        <div className='HeaderContainer'>
+            <div className='headerTitle '>
+            <img src='#' alt="" />
+                <h4 className='Navheading'>QuizMania</h4>
+            </div>
+            <div className='menu-iteam'>
+                <Link to='/'>Home</Link>
+                <Link to='/statistic'>Statistic</Link>
+                <Link to='/blog'>Blog</Link>
+                <Link to='/about'>About</Link>
+
+            </div>
         </div>
     );
 };
